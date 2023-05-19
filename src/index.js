10540a54a63c8fb251686d7cfcd9681616fe00a3
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-19 15:57:00 -0300

    tela de login finalizada

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Login from "./containers/Login";
+import GlobalStyles from "./styles/globalStyles";
 
 ReactDOM.render(
-  <Login />,
-
+  <>
+    <Login />,
+    <GlobalStyles />
+  </>,
   document.getElementById("root")
 );
