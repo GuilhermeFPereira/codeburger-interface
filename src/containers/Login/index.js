6d6cf739e03ecc13fa87d42b773988da58245b4d
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-22 15:22:04 -0300

    useContext

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -8,6 +8,7 @@ import * as Yup from 'yup'
 import LoginImg from '../../assets/login-image.svg'
 import Logo from '../../assets/logo.svg'
 import Button from '../../components/Button'
+import { useUser } from '../../hooks/UserContext'
 import api from '../../services/api'
 import {
   Container,
@@ -20,6 +21,10 @@ import {
 } from './styles'
 
 function Login() {
+  const users = useUser()
+
+  console.log(users)
+
   const schema = Yup.object().shape({
     email: Yup.string()
       .email('Digite um e-mail válido')
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 
-// import Login from './containers/Login'
-import Register from './containers/Register'
+import Login from './containers/Login'
+// import Register from './containers/Register'
+import { UserProvider } from './hooks/UserContext'
 import GlobalStyles from './styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
-    <Register />,
+    <UserProvider>
+      <Login />
+    </UserProvider>
     <ToastContainer autoClose={2000} theme="colored" />
     <GlobalStyles />
   </>
