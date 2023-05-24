ae3638559bf3d2ea01ba36417292b09c5e4d3846
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-24 19:31:17 -0300

    estiilizando carrinho

---
+++ src/components/CartItems/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { useCart } from '../../hooks/CartContext'
+import formatCurrency from '../../utils/formatCurrency'
+import { Container, Header, Body, EmptyCart } from './styles'
+
+export function CartItems() {
+  const { cartProducts } = useCart()
+  return (
+    <Container>
+      <Header>
+        <p></p>
+        <p>Items</p>
+        <p>Preço</p>
+        <p style={{ paddingRight: 30 }}>Quantidade</p>
+        <p>Total</p>
+      </Header>
+
+      {cartProducts && cartProducts.length > 0 ? (
+        cartProducts.map(product => (
+          <Body key={product.id}>
+            <img src={product.url} />
+            <p>{product.name}</p>
+            <p>{formatCurrency(product.price)}</p>
+            <p>{product.quantity}</p>
+            <p>{formatCurrency(product.quantity * product.price)}</p>
+          </Body>
+        ))
+      ) : (
+        <EmptyCart>Carrinho Vazio</EmptyCart>
+      )}
+    </Container>
+  )
+}
