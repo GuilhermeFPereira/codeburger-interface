fbaf9e45796b13016965ee1066b12dfeb0eb632c
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-26 22:12:01 -0300

    criando menu de selecao de status

--- src/containers/Admin/Orders/order-status.js
+++ src/containers/Admin/Orders/order-status.js
@@ -1,9 +1,10 @@
 const status = [
-  { id: 1, label: 'Pedido realizado', value: 'Pedido realizado' },
-  { id: 2, label: 'Em Preparação', value: 'Em Preparação' },
-  { id: 3, label: 'Pedido Pronto', value: 'Pedido Pronto' },
-  { id: 4, label: 'Pedido á caminho', value: 'Pedido á caminho' },
-  { id: 5, label: 'Entregue', value: 'Entregue' }
+  { id: 1, label: 'Todos', value: 'Todos' },
+  { id: 2, label: 'Pedido realizado', value: 'Pedido realizado' },
+  { id: 3, label: 'Em Preparação', value: 'Em Preparação' },
+  { id: 4, label: 'Pedido Pronto', value: 'Pedido Pronto' },
+  { id: 5, label: 'Pedido á caminho', value: 'Pedido á caminho' },
+  { id: 6, label: 'Entregue', value: 'Entregue' }
 ]
 
 export default status
