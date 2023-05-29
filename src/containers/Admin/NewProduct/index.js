6cccabb59f388d1909d232b8855a1113a426f63a
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-29 20:07:48 -0300

    form de adicao de produto

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import ReactSelect from 'react-select'
 
+import { Button } from '../../../components'
 import api from '../../../services/api'
-import { Container } from './styles'
+import { Container, Label, Input } from './styles'
 
 function NewProduct() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
+
   useEffect(() => {
     async function loadOrders() {
       const { data } = await api.get('products')
@@ -14,7 +20,20 @@ function NewProduct() {
 
   return (
     <Container>
-      <div>ok</div>
+      <form noValidate>
+        <Label>Nome</Label>
+        <Input type="text" {...register('name')} />
+
+        <Label>Preco</Label>
+        <Input type="number" {...register('price')} />
+
+        <Label>Upload da imagem</Label>
+        <Input type="file" accept="image/png, image/jpeg" />
+
+        <ReactSelect />
+
+        <Button>Adicionar Produtos</Button>
+      </form>
     </Container>
   )
 }
